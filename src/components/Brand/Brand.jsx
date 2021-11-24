59a6869af47e1115baa425a7fe728098a4c6ca8a
asraful <iasraful107@gmail.com>
2021-11-24 21:57:07 +0600

    added

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './Brand.css';
+import { atlassian, dropbox, google, shopify, slack } from './Imports';
 const Brand = () => {
     return (
-        <div>
-            brad
-        </div>
+       <div className="gpt3__brand section__padding">
+            <div>
+                <img src={google} alt="" />
+            </div>
+            <div>
+                <img src={shopify} alt="" />
+            </div>
+            <div>
+                <img src={slack} alt="" />
+            </div>
+            <div>
+                <img src={dropbox} alt="" />
+            </div>
+            <div>
+                <img src={atlassian} alt="" />
+            </div>
+       </div>
     );
 };
 
